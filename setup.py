022b781a7ae2eff92ff75e182e8862725f37a0f6
Zachary Windom <zww4855@gmail.com>
2022-10-11 03:44:11 -0400

    modified setup

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     # Which Python importable modules should be included when your package is installed
     # Handled automatically by setuptools. Use 'exclude' to prevent some specific
     # subpackage(s) from being added, if needed
-    packages=find_packages(),
+    packages=find_packages(numpy, scipy, os, sys, math, orderedCollections),
 
     # Optional include package data to ship with your package
     # Customize MANIFEST.in if the general case does not suit your needs
